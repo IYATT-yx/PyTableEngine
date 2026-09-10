4a5535b12f36d446c23613a5dbbc1514333a6999
IYATT-yx <iyatt@iyatt.com>
2026-09-10 22:04:26 +0800

    更新地址类名

--- core/constants.py
+++ core/constants.py
@@ -20,5 +20,6 @@ class Command:
     argv0 = os.path.abspath(sys.argv[0])
     executableCommandString = argv0 if Path.isPackaged else sys.executable + ' ' + argv0
 
-class Plugin:
+class Site:
+    pypi =  'https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple'
     repositoryIndex = 'https://raw.githubusercontent.com/IYATT-yx/PyTableEnginePluginRepository/main/index.json'
